173c99ee86d6bbf9c3d38c41c11fa805f3c67a8c
klin1344 <kevlin97@gmail.com>
2019-09-21 23:15:31 -0500

    Add route for testing to nuke al workers except Erica and add completion time to work order form

--- workorder/src/Technician/TechnicianForm.js
+++ workorder/src/Technician/TechnicianForm.js
@@ -31,7 +31,6 @@ class TechnicianForm extends React.Component {
 
     }
     onSubmit = () => {
-
         axios.post('http://localhost:8000/worker_submission', {
             name: this.form.name,
             phone_number: this.form.phone,
@@ -82,7 +81,7 @@ class TechnicianForm extends React.Component {
                             </Form.Control>
                         </Form.Group>
 
-                        <Button variant="primary" onClick={click => this.onSubmit()}>
+                        <Button variant="primary" type="submit" onClick={click => this.onSubmit()}>
                             Submit
             </Button>
                     </Form>
